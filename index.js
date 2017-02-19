011b2726a12ded795a3df85847a17ed27aa4ba5a
maxkoryukov <maxkoryukov@yandex.ru>
2017-02-19 16:22:43 +0500

    simplified

--- index.js
+++ index.js
@@ -3,11 +3,30 @@
 module.exports = {
 	// Map of hooks
 	hooks: {
-		"init": require("./patch")
+		"init": function(a, b, c) {
+			console.log("PLUGIN ========================================")
+			console.log(a, b, c)
+
+			console.log(
+				a,
+				typeof a,
+				a.toString(),
+				a.constructor
+			)
+			return a
+		}
 	},
 
 	// Map of new blocks
-	blocks: {},
+	blocks: {
+
+		tag1: {
+            process: function(block, b, c, d) {
+            	console.log("***************************************", block,b,c,d)
+                return "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& Hello "+block.body+", How are you?";
+            }
+        }
+	},
 
 	// Map of new filters
 	filters: {}
